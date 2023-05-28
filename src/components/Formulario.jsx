3e36e0df4a480626def90fe42ffab7ca3d224cbe
Emanuel Ruiz <ruizemanuelok@gmail.com>
2023-05-28 05:12:26 -0300

    guardar value dentro de un array

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,18 +1,34 @@
-import { Form, Button } from 'react-bootstrap';
-import Lista from './Lista';
+import { Form, Button } from "react-bootstrap";
+import Lista from "./Lista";
+import { useState } from "react";
 
 const Formulario = () => {
-    return (
-        <>
-        <Form>
+  const [Tarea, setTarea] = useState("");
+  const [Tareas, setTareas] = useState([]);
+  const hundleSubmit = (e) => {
+    e.preventDefault();
+    setTareas([...Tareas, Tarea]);
+    setTarea("");
+  };
+  return (
+    <>
+      <Form onSubmit={hundleSubmit}>
         <Form.Group className="mb-3 d-flex" controlId="tarea">
-          <Form.Control className='mx-2' type="text" placeholder="ingrese una tarea" />
-          <Button variant='info'>Enviar</Button>
+          <Form.Control
+            className="me-2"
+            type="text"
+            placeholder="ingrese una tarea"
+            onChange={(e) => setTarea(e.target.value)}
+            value={Tarea}
+          />
+          <Button type="submit" variant="info">
+            Enviar
+          </Button>
         </Form.Group>
       </Form>
       <Lista></Lista>
-        </>
-    );
+    </>
+  );
 };
 
 export default Formulario;
