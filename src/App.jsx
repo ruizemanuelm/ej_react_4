1c99936feb8497b504290682d5207312eb780149
Emanuel Ruiz <ruizemanuelok@gmail.com>
2023-05-28 04:00:34 -0300

    creacion de titulo y footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
-import './App.css'
+import { Container } from "react-bootstrap";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-
-
   return (
     <>
-      
+      <Container className="my-5  main">
+        <section className="text-light">
+        <h1 className=" text-center display-3">Lista de tareas</h1>
+        <hr />
+        </section>
+      </Container>
+      <footer className="bg-dark p-3 text-danger text-center">
+        <p>Todos los derechos reservados </p>
+      </footer>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
